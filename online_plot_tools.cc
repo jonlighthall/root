cbaf2a283626adaf364e9ccb2ebc8585e8ec0852
Jon Lighthall <jon.lighthall@gmail.com>
2014-08-29 21:12:56 +0000

    updated online macros

--- online_plot_tools.cc
+++ online_plot_tools.cc
@@ -1,6 +1,4 @@
-
-
-
+/*---------------------------------------------------------------------------------------------*/
 //Copied from fit.cc for use with Scarlet at Argonne
 
 void hdr(Char_t *histname,Float_t xmin=-999999.,Float_t xmax=999999.,Float_t ymin=-999999.,
@@ -29,4 +27,58 @@ void hplotalllow(Char_t *histin, Char_t *suffix="", Int_t style=7, Int_t size=1,
   plotalllow(histin,suffix,style,size,color);
 }
 
+/*---------------------------------------------------------------------------------------------*/
+//Copied from util_new.cc for use with Scarlet at Argonne
+void d1(Char_t *histname)
+{
+  hup();
+  if( c1==0 ) mkCanvas();
+  else c1->cd();
+  draw(histname);
+}
 
+void d2(Char_t *histname)
+{
+  hup();
+  if( c1==0 ) mkCanvas();
+  else c1->cd();
+  draw2(histname);
+}
+
+void zap(Char_t *histname)
+{
+  switch(whatis(histname)) {
+  case 0:
+    cout << "object " << histname << " was not found."<<endl;
+    break;
+  case 1:
+    TH1F *hist=(TH1F*) gROOT->FindObject(histname);
+    break;
+  case 2:
+    TH1D *hist=(TH1D*) gROOT->FindObject(histname);
+    break;
+  case 3:
+    TH2F *hist=(TH2F*) gROOT->FindObject(histname);
+    break;
+  case 4:
+    TH2D *hist=(TH2D*) gROOT->FindObject(histname);
+    break;
+  case 5:
+    TH3F *hist=(TH3F *) gROOT->FindObject(histname);
+    break;
+  case 6:
+    TProfile *hist=(TProfile *) gROOT->FindObject(histname);
+    break;
+  }
+  hist->Reset();
+}
+
+void zapall()
+{
+  TList *hlist=gDirectory->GetList();
+  TIterator *hiterator=hlist->MakeIterator();
+  TH1 *htemp;
+  while (htemp= (TH1 *) hiterator->Next()) {
+    if (htemp->InheritsFrom("TH1")) htemp->Reset();
+  }
+}
