7caff70268d8ef44b11412b17dc7381534151928
Jon Lighthall <jon.lighthall@gmail.com>
2014-09-03 23:30:28 +0000

    working position calculation at mask

--- generator.cc
+++ generator.cc
@@ -5,6 +5,8 @@
  *       A set of macros simulating data
  */
 
+#include "TMath.h"
+
 void testRandom(Int_t nrEvents=500000000)
 {
   TRandom *r1=new TRandom();
@@ -61,42 +63,149 @@ void testRandom2(Int_t nrEvents=10e+08, double mean = 0, double sigma = 100)
   gfit("h2");
 }
 
+
+Double_t sigma_x=0.607956845;//for 90% in a 1mm radius
+Double_t sigma_y=sigma_x;
+Double_t offset_x=0, offset_y=0;
+//TH2D *hbeam=0;
+//TH2D *htheta=0;
+
+void createbeam(Double_t set_offset_x=0, Double_t set_offset_y=0, Double_t set_sigma_x=0.607956845, Double_t set_sigma_y=0.607956845)
+{
+  sigma_x=set_sigma_x;
+  sigma_y=set_sigma_y;
+  offset_x=set_offset_x;
+  offset_y=set_offset_y;
+
+  if (gROOT->FindObject("hbeam"))
+    gROOT->FindObject("hbeam")->Delete();   
+  TH2D *hbeam=new TH2D("hbeam","Beam Spot",500,-10,10,500,-10,10);
+  hbeam->SetXTitle("x-position (mm)");
+  hbeam->SetYTitle("y-position (mm)");
+  source(1e6);
+  drpjxy("hbeam");
+}
+
+Float_t z_mask=637.0;
+Float_t z_window=z_mask+3.25;
+Float_t z_A2=z_window+6.35+12.7+23.6;
+Float_t z_A1=z_A2+36.8;//anode separation
+Float_t delta_z=0.125*25.4;//plane separation
+
+Float_t z_Y2=z_A2-delta_z;
+Float_t z_X2=z_A2+delta_z;
+Float_t z_Y1=z_A1-delta_z;
+Float_t z_X1=z_A1+delta_z;
+
+
+Double_t x=0,y=0;//point on target
+Double_t theta=0;//scattering angle
+Double_t phi=0;//azimuthal angle
+Double_t X=0, Y=0, Z=0;//position
+Double_t r=0,rho=0;//radius
+Bool_t doprint=kFALSE;
+
+
+void trace_r(Double_t z_start,Double_t theta_start, Double_t phi_start)
+{//given z, theta, phi; calcuate r
+  r=z_start/(TMath::Cos(theta_start*TMath::DegToRad()));
+  
+  
+  if(doprint) {
+    printf(" Emmission angle (theta,phi)=(%f,%f)\n",theta_start,phi_start);
+    printf("  Current position is Z=%f\n",Z);
+    printf("  Radius is           r=%f\n",r);
+  }
+ }
+
+void trace_x(Double_t x_start, Double_t theta_start, Double_t phi_start)
+{
+X=r*(TMath::Sin(theta_start*TMath::DegToRad()))*(TMath::Cos(phi_start*TMath::DegToRad()));
+  
+  if(doprint) {
+printf("  X-position is %f (relative), with offset %f\n",X,x);
+  }
+  X+=x;  
+
+if(doprint) {
+printf("  X-position is %f (absolute)\n",X);
+  }
+}
+
+void trace_y(Double_t y_start, Double_t theta_start, Double_t phi_start)
+{
+    Y=r*(TMath::Sin(theta_start*TMath::DegToRad()))*(TMath::Sin(phi_start*TMath::DegToRad()));
+  
+  if(doprint) {
+printf("  Y-position is %f (relative), with offset %f\n",Y,y);
+  }
+  Y+=y;  
+
+if(doprint) {
+printf("  Y-position is %f (absolute)\n",Y);
+ }
+}
+
+
+
+
 void source(Int_t nevents=1000)
 {
-  //TString histnames[20]={"hbeamspot","htheta"}
+  //TObjString histnames[20]={"hbeam","htheta"}
   //beam spot
   TRandom *rx=new TRandom();//for x-position of beam spot
   TRandom *ry=new TRandom();//for y-position of beam spot
-  Double_t x=0,y=0;
-  Double_t sigma_x=0.607956845;//for 90% in a 1mm radius
-  Double_t sigma_y=sigma_x;
-  Double_t offset_x=0, offset_y=0;
-  if (gROOT->FindObject("hbeamspot"))
-    gROOT->FindObject("hbeamspot")->Delete();   
-  TH2D *hbeamspot=new TH2D("hbeamspot","Beam Spot",500,-10,10,500,-10,10);
-  hbeamspot->SetXTitle("x-position (mm)");
-  hbeamspot->SetYTitle("y-position (mm)");
-
+ 
+ 
+  //scattering angle
   TRandom *rtheta=new TRandom();//for scattering angle 
   if (gROOT->FindObject("htheta"))
     gROOT->FindObject("htheta")->Delete();    
   TH1D *htheta=new TH1D("htheta","Emission Angle",500,0,180);
-  Double_t theta=0;
+  
   Double_t theta_min=7;
   Double_t theta_max=-theta_min;
   Double_t theta_center=30;
   theta_min+=theta_center;
   theta_max+=theta_center;
  
+  //azimuthal angle
+  TRandom *rphi=new TRandom();
   
+  Double_t phi_min=0;
+  Double_t phi_max=360;
+  if (gROOT->FindObject("hphi"))
+    gROOT->FindObject("hphi")->Delete();    
+  TH1D *hphi=new TH1D("hphi","Emission Angle",500,0,180);
+
+  //Ray
+  
+  Bool_t hit=kFALSE;
+
   for (Int_t i=0; i<nevents; i++) {
     x=rx->Gaus(offset_x,sigma_x);
     y=rx->Gaus(offset_y,sigma_y);
-    hbeamspot->Fill(x,y);
+    hbeam->Fill(x,y);
     theta=rtheta->Uniform(theta_min,theta_max);
     htheta->Fill(theta);
-    if(i%500000==0)
+    phi=rphi->Uniform(phi_min,phi_max);
+    hphi->Fill(phi);
+    
+    if(i%500000==0){
       printf("%5.1f\%: %d events generated\n",(double)i/nevents*100,i);
-  }
-  dr("htheta");  
+      doprint=kTRUE;
+    }
+    else
+      doprint=kFALSE;
+    
+    Z=z_mask;
+    trace_r(Z,theta,phi);
+    trace_x(x,theta,phi);
+    trace_y(y,theta,phi);
+    rho=TMath::Sqrt((TMath::Power(X,2))+(TMath::Power(X,2)));
+    if(doprint)
+      printf("  rho=%f\n",rho);
+    
+   
+}
 }
