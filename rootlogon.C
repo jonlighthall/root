35706031012be1717b9edfbcb9cde4e2d3263401
Jon Lighthall <jon.lighthall@gmail.com>
2017-03-30 18:07:42 -0400

    Add “historic” macros. Remove directory names.
    References to the original ROOT macros from which this suite of macros is originally derived are included. Path names are removed. .rootrc should up updated accordingly. Reference to temp.cc is a reminder to purge that file.

--- rootlogon.C
+++ rootlogon.C
@@ -1,9 +1,17 @@
 {
+  printf("Using .root_macros/scripts version.\n");
+  //------------------------------------
+  //Historic settings
+  //------------------------------------
+  //gROOT->LoadMacro("/net/bavarians/utilities/util.cc");
+  //gROOT->LoadMacro("/net/bavarians/utilities/dsp.cc");
+  //gROOT->LoadMacro("/net/helios/B12/helios_b12_plottools.cc");
+
   //------------------------------------
   //Load general macros and settings
   //------------------------------------
   //gROOT->LoadMacro("C:/root/macros/fit.cc");
-  gROOT->LoadMacro("scripts/fit.cc");  
+  gROOT->LoadMacro("fit.cc");  
   //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/fit.cc");
   //gROOT->LoadMacro("scripts/online_plot_tools.cc");
   setdisplay(); //calls function in fit.cc
@@ -14,7 +22,8 @@
   //Load specfic macros and settings
   //------------------------------------
   //gROOT->LoadMacro("C:/root/macros/load_and_plot.cc");
-  gROOT->LoadMacro("scripts/load_and_plot.cc");
+    gROOT->LoadMacro("load_and_plot.cc");
+  //gROOT->LoadMacro("temp.cc");
   //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/load_and_plot.cc");
   gStyle->SetStatH(0.05);//reduces the size of the stat box
 
@@ -26,7 +35,7 @@
   //------------------------------------
   //Load PGAC simulation  
   //------------------------------------
-  gROOT->LoadMacro("scripts/generator.cc");
+  //gROOT->LoadMacro("generator.cc");
   //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/generator.cc");
   //setbeam(0,0);
   //setangles(0,4.5,0,360);
