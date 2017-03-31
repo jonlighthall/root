c6cd12296743d3507fa0bec58065a8ec666d6ce4
Jon Lighthall <jon.lighthall@gmail.com>
2017-03-31 17:33:35 -0400

    Update non-functions
    Update printed message, add instructions, remove redundant commented-out lines

--- rootlogon.C
+++ rootlogon.C
@@ -1,5 +1,7 @@
 {
-  printf("Using .root_macros/scripts version.\n");
+  printf("GitHub version of rootlogon.C being used.\n");
+  //Set macro location in .rootrc
+
   //------------------------------------
   //Historic settings
   //------------------------------------
@@ -10,9 +12,7 @@
   //------------------------------------
   //Load general macros and settings
   //------------------------------------
-  //gROOT->LoadMacro("C:/root/macros/fit.cc");
   gROOT->LoadMacro("fit.cc");  
-  //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/fit.cc");
   //gROOT->LoadMacro("scripts/online_plot_tools.cc");
   setdisplay(); //calls function in fit.cc
   settime(true);
@@ -21,10 +21,8 @@
   //------------------------------------
   //Load specfic macros and settings
   //------------------------------------
-  //gROOT->LoadMacro("C:/root/macros/load_and_plot.cc");
-    gROOT->LoadMacro("load_and_plot.cc");
+  gROOT->LoadMacro("load_and_plot.cc");
   //gROOT->LoadMacro("temp.cc");
-  //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/load_and_plot.cc");
   gStyle->SetStatH(0.05);//reduces the size of the stat box
 
   //------------------------------------
@@ -36,7 +34,6 @@
   //Load PGAC simulation  
   //------------------------------------
   //gROOT->LoadMacro("generator.cc");
-  //gROOT->LoadMacro("/home/emma/offline/lighthall/rootana/analysis/scripts/generator.cc");
   //setbeam(0,0);
   //setangles(0,4.5,0,360);
   //setresm(0.6);
